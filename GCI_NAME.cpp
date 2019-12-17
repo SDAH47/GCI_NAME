897594a28a653f5f7eedbf178f6cbbb2e79d9222
SDAH47 <58889389+SDAH47@users.noreply.github.com>
2019-12-17 18:41:03 +0600

    Add files via upload

---
+++ GCI_NAME.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void);
+
+int main() {
+	using namespace std;
+	
+	cout << "My GCI Username: SDAH" << endl;
+	
+	return 0;
+}
---
+++ makefile.mk
@@ -0,0 +1,4 @@
+GCI_NAME.cpp:
+	g++ -c D:\GCI_NAME\GCI_NAME.cpp
+	g++ -o GCI_NAME GCI_NAME.o
+	./GCI_NAME
